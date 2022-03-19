19c28ae667aed709aad23ec034689109d8e0c5c3
ObcbO <m17089494917@163.com>
2022-03-19 10:05:32 +0800

    awa

--- app.js
+++ app.js
@@ -1,43 +1,50 @@
 const mineflayer = require('mineflayer')
 const {
-    pathfinder,
-    Movements,
-    goals
-} = require('mineflayer-pathfinder')
-const {
     send
 } = require('process')
 const pvp = require('mineflayer-pvp').plugin
 const autoEat = require('mineflayer-auto-eat')
+const pathfinder = require('mineflayer-pathfinder').pathfinder
+const Movements = require('mineflayer-pathfinder').Movements
+const { GoalNear } = require('mineflayer-pathfinder').goals
 
 const bot = mineflayer.createBot({
     host: 'localhost',
     username: 'ChinaTrump',
     port: 25565,
 })
-//让所有js都可以访问bot
-global.bot =bot
+//让所有js都可以访问b
+global.bot = bot
+global.pathfinder = pathfinder
+global.Movements = Movements
+global.GoalNear = GoalNear
 
 //my
 const input = require('./input')
 const mcs = require('./console')
 
-//load plugin
+//load plugins
 bot.loadPlugin(autoEat)
+bot.loadPlugin(pathfinder)
 
 //初始化
-
+global.onwer
+// Require MCData and set pathfinder movements
+global.mcData = require('minecraft-data')(bot.version)
 
 //控制台输入
 input.sendmessage()
 
 //控制台信息显示
-bot.on('message', (game_info) => {
+bot.on('message', async (game_info) => {
     mcs.info(game_info)
 })
 //控制台聊天显示
-bot.on('chat', (username, message) => {
+bot.on('chat', async (username, message) => {
     mcs.chat(username, message)
+    if (owner == username) {
+        input.command(message)
+    }
 })
 bot.on('kicked', function (reason) {
     mcs.waring("BOT IS KICKED. Reason:" + reason)
@@ -58,7 +65,7 @@ bot.on('autoeat_started', () => {
 bot.on('autoeat_stopped', () => {
     console.log('Auto Eat stopped!')
 })
-bot.on('health', () => {
+bot.on('health', async () => {
     if (bot.food === 20) {
         bot.autoEat.disable()
     } else {
--- input.js
+++ input.js
@@ -1,22 +1,20 @@
 const mcs = require('./console')
-const {
-    pathfinder,
-    Movements,
-    goals
-} = require('mineflayer-pathfinder')
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 })
 module.exports = {
-    sendmessage
+    sendmessage,
+    command
 }
 
 var noc
 
+bot.loadPlugin(pathfinder)
+
 function sendmessage() {
-    readline.question(``, msg => {
-        if (msg.slice(0,1) == ".") {
+    readline.question(``, async msg => {
+        if (msg.slice(0, 1) == ".") {
             noc = msg.slice(msg.trim().split(" ")[0].length + 1)
             command(msg.trim().split(" "))
         } else bot.chat(msg)
@@ -24,16 +22,23 @@ function sendmessage() {
     })
 }
 
-function command (text) {
+function announceArrived() {
+    const botPosition = bot.entity.position
+    mcs.cmd(`Arrived, "Position " + bot.entity.position`)
+}
+
+function command(text) {
     switch (text[0]) {
         case ".go":
-            bot.pathfinder.goto(text[1], text[2], text[3])
+            bot.pathfinder.setMovements(new Movements(bot, mcData))
+            bot.pathfinder.goto(new GoalNear(text[1], text[2], text[3])).then(announceArrived)
             break
         case ".help":
             mcs.cmd("----- HELP 帮助 -----")
             mcs.cmd(".go x y z  --前往某个位置")
             mcs.cmd(".help  --查看帮助")
             mcs.cmd(".now [type/help]  --查看机器人信息")
+            mcs.cmd(".owner [玩家]  --主人")
             mcs.cmd(".say 消息  --发送消息")
             mcs.cmd("-------- END --------")
             break
@@ -71,6 +76,16 @@ function command (text) {
                     mcs.cmd("Exp " + bot.experience.points)
                     mcs.cmd("Time " + bot.oxygenLevel)
                     mcs.cmd("Position " + bot.entity.position)
+            }
+            break
+        case ".owner":
+            if (noc !== null) {
+                onwer = noc
+                mcs.cmd("设置成功 Owner: " + owner)
+            } else {
+                if (owner === null) {
+                    mcs.error("BOT还没有设置主人 请使用 '/owner 玩家' 来设置")
+                } else mcs.cmd("Owner: " + owner)
             }
             break
         case ".say":
